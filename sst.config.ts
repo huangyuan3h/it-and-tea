c7021b831f2ec0352da7f0df7502d80097fde222
Yuan Huang <huangyuan3h@gmail.com>
2024-08-30 11:43:33 +0800

    update the deploy

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,9 @@
 import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+
+const certArn =
+  "arn:aws:acm:us-east-1:319653899185:certificate/dde24c52-09e4-4058-b1d0-2a7769f24e3a";
 
 export default {
   config(_input) {
@@ -10,7 +14,16 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
+      const customDomain = {
+        domainName: "www.it-t.xyz",
+        isExternalDomain: true,
+        cdk: {
+          certificate: Certificate.fromCertificateArn(stack, "MyCert", certArn),
+        },
+      };
+
       const site = new NextjsSite(stack, "site", {
+        customDomain: stack.stage === "production" ? customDomain : undefined,
         environment: {
           NEXT_PUBLIC_BACKEND_API: process.env.NEXT_PUBLIC_BACKEND_API ?? "",
         },
