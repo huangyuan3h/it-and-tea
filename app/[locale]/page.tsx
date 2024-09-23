28c15354dc29e3070ba3065c21503c60ffc54b0e
Yuan Huang <huangyuan3h@gmail.com>
2024-09-23 21:20:43 +0800

    update the hero banner

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/header";
 import { useTranslations } from "next-intl";
 import { TopSlider } from "./components/top-slider";
 import { Text2Speech } from "./components/tts/Text2Speech";
+import Hero from "./components/hero-video";
 
 export default function Home({
   params: { locale },
@@ -15,6 +16,7 @@ export default function Home({
   return (
     <main>
       <Header />
+      <Hero />
       <Text2Speech />
       <TopSlider />
       <div className="container">
