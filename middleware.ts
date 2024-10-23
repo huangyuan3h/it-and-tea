6370ac423e7edba45ab5b6427652077e39e6ea94
Yuan Huang <huangyuan3h@gmail.com>
2024-10-23 15:21:03 +0800

    update the matcher for middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,12 @@
 import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
-  // A list of all locales that are supported
   locales: ["en", "zh", "fr"],
 
-  // Used when no locale matches
   defaultLocale: "en",
 });
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(en|zh|fr)/:path*"],
+  matcher: ["/(about|policy)?", "/(en|zh|fr)/:path*"],
 };
